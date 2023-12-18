3cb4f1105342df806dc7bb47f8d7247914e72a62
Pratishtha33 <singhpratishtha790@gmail.com>
2023-12-18 08:40:20 +0530

    updates

---
+++ handler/handler_test.go
@@ -0,0 +1,98 @@
+package handler
+
+import (
+	"errors"
+	"testing"
+
+	"gofr.dev/model"
+	"gofr.dev/pkg/gofr"
+)
+
+// MockLeaveStore is a mock implementation of the Leave data store interface for testing.
+type MockLeaveStore struct{}
+
+func (s *MockLeaveStore) GetByID(ctx *gofr.Context, id string) (*model.Leave, error) {
+	
+	return nil, nil
+}
+
+func (s *MockLeaveStore) Create(ctx *gofr.Context, leave *model.Leave) (*model.Leave, error) {
+	// logic for Create
+	return nil, nil
+}
+
+func (s *MockLeaveStore) Update(ctx *gofr.Context, leave *model.Leave) (*model.Leave, error) {
+	// logic for Update
+	return nil, nil
+}
+
+func (s *MockLeaveStore) Delete(ctx *gofr.Context, id int) error {
+	// logic for Delete
+	return nil
+}
+
+func TestGetByID(t *testing.T) {
+	// Create a new instance of the handler with a mock Leave data store.
+	h := New(&MockLeaveStore{})
+
+	// Create a mock context.
+	ctx := &gofr.Context{}
+
+	// Call the GetByID method.
+	_, err := h.GetByID(ctx)
+
+	// Check if there is no error.
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+}
+
+func TestCreate(t *testing.T) {
+	// Create a new instance of the handler with a mock Leave data store.
+	h := New(&MockLeaveStore{})
+
+	// Create a mock context.
+	ctx := &gofr.Context{}
+
+	// Call the Create method.
+	_, err := h.Create(ctx)
+
+	// Check if there is no error.
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	// Create a new instance of the handler with a mock Leave data store.
+	h := New(&MockLeaveStore{})
+
+	// Create a mock context.
+	ctx := &gofr.Context{}
+
+	// Call the Update method.
+	_, err := h.Update(ctx)
+
+	// Check if there is no error.
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	// Create a new instance of the handler with a mock Leave data store.
+	h := New(&MockLeaveStore{})
+
+	// Create a mock context.
+	ctx := &gofr.Context{}
+
+	// Call the Delete method.
+	_, err := h.Delete(ctx)
+
+	// Check if there is no error.
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+}
+
+
